2db30d4c6a454098db5bab62a7c77b834e590a7d
Shrayashd <shrayash000@gmail.com>
2025-05-20 13:40:46 +0545

    Index page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,22 +26,22 @@ class DatabaseSeeder extends Seeder
             ['site_phone', '9898989898'],
             ['site_email', 'admin@opm.com'],
             ['site_phone_two', '9898989898'],
-            ['site_email_two', 'admin@opm.com'],
+            ['site_email_two', 'admin@goreto.com'],
             ['site_location', 'Shukra Bhawan, Thamel Marg, Kathmandu'],
             ['site_location_url', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113024.3948482901!2d85.15919149726561!3d27.7169053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19d25661482b%3A0x2f1639d4d0ba9959!2sAdventure%20Planner%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1683524016919!5m2!1sen!2snp'],
             ['site_facebook', 'https://paradiseinfo.tech/'],
             ['site_youtube', 'https://paradiseinfo.tech/'],
             ['site_instagram', 'https://paradiseinfo.tech/'],
             ['site_linkedin', 'https://paradiseinfo.tech/'],
-            ['site_topbar', 'Ohm Pharmaceuticals'],
-            ['site_copyright', 'Ohm Pharmaceuticals'],
+            ['site_topbar', 'Goreto'],
+            ['site_copyright', 'Goreto'],
 
-            ['homepage_title', 'Ohm Pharmaceuticals'],
+            ['homepage_title', 'Goreto'],
             ['homepage_image', null],
-            ['homepage_description', 'Ohm Pharmaceuticals'],
-            ['homepage_seo_title', 'Ohm Pharmaceuticals'],
-            ['homepage_seo_description', 'Ohm Pharmaceuticals'],
-            ['homepage_seo_keywords', 'Ohm Pharmaceuticals'],
+            ['homepage_description', 'Goreto'],
+            ['homepage_seo_title', 'Goreto'],
+            ['homepage_seo_description', 'Goreto'],
+            ['homepage_seo_keywords', 'Goreto'],
             ['homepage_seo_schema', null],
 
             ['categorysmalltitle', 'Explore'],
@@ -62,11 +62,11 @@ class DatabaseSeeder extends Seeder
             ['reviewinfo', null],
             ['reviews', null],
 
-            ['team_title', 'Ohm Pharmaceuticals'],
-            ['team_description', 'Ohm Pharmaceuticals'],
+            ['team_title', 'Goreto'],
+            ['team_description', 'Goreto'],
 
             ['blog_title', 'Our Blogs'],
-            ['blog_description', 'Ohm Pharmaceuticals'],
+            ['blog_description', 'Goreto'],
         ];
 
         foreach ($items as $item) {
@@ -77,10 +77,10 @@ class DatabaseSeeder extends Seeder
         }
 
         // Create super admin only if it doesn't exist
-        if (!User::where('email', 'admin@opl.com')->exists()) {
+        if (!User::where('email', 'admin@goreto.com')->exists()) {
             User::create([
                 'name' => 'Super Admin',
-                'email' => 'admin@opl.com',
+                'email' => 'admin@goreto.com',
                 'password' => Hash::make('Nepal@123'),
             ]);
         }
