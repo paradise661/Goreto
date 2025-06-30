473a7c96cc52dc973eb53e6af90b13d86551f108
Shrayashd <shrayash000@gmail.com>
2025-06-30 17:35:29 +0545

    Title updated

--- resources/views/admin/member/edit.blade.php
+++ resources/views/admin/member/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin.master')
-@section('title', 'Edit ' . $member->name . ' - Ohm Pharmaceuticals')
+@section('title', 'Edit ' . $member->name . ' - Goreto')
 
 @section('content')
     @include('admin.includes.message')
