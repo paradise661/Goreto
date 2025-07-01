773604e14b7bacc75d8b4c9e64f89153992de835
Shrayashd <shrayash000@gmail.com>
2025-07-01 17:01:55 +0545

    Fixing view frontend

--- resources/views/admin/review/edit.blade.php
+++ resources/views/admin/review/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin.master')
-@section('title', 'Edit ' . $review->name . ' - Ohm Pharmaceuticals')
+@section('title', 'Edit ' . $review->name . ' - Goreto')
 
 @section('content')
     @include('admin.includes.message')
